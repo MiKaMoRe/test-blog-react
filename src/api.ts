88ae8537f7ee61f654ea3ee27e024faa6a8cf32a
mikamore <kim.roman-2002@yandex.ru>
2024-09-19 23:21:26 +0300

    Move api mock data to file

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
+import { posts } from "./api_mockdata/posts"
 
 export const api = axios.create({
   baseURL: process.env.BACKEND_URL ?? "http://localhost:8000",
@@ -7,50 +8,5 @@ export const api = axios.create({
 
 const mock = new MockAdapter(api, { delayResponse: 1000 });
 
-mock.onGet("/posts").reply(200, [
-  {
-    id: 1,
-    title: "Best post in the world",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
-    slug: "post1",
-    created_at: "202212012004",
-    updated_at: "202212012004",
-  },
-  {
-    id: 2,
-    title: "Best post in the world",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
-    slug: "post2",
-    created_at: "202212012004",
-    updated_at: "202212012004",
-  },
-  {
-    id: 3,
-    title: "Best post in the world",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
-    slug: "post3",
-    created_at: "202212012004",
-    updated_at: "202212012004",
-  },
-  {
-    id: 4,
-    title: "Best post in the world",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
-    slug: "post4",
-    created_at: "202212012004",
-    updated_at: "202212012004",
-  },
-  {
-    id: 5,
-    title: "Best post in the world",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
-    slug: "post5",
-    created_at: "202212012004",
-    updated_at: "202212012004",
-  },
-]);
+mock.onGet("/posts", { params: { limit: 30, skip: 0 } }).reply(200, posts);
+mock.onGet("/posts", { params: { limit: 30, skip: 1 } }).reply(200, posts.slice(2));
---
+++ src/api_mockdata/posts.ts
@@ -0,0 +1,47 @@
+export const posts = [
+  {
+    id: 1,
+    title: "Best post in the world",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
+    slug: "post1",
+    created_at: "202212012004",
+    updated_at: "202212012004",
+  },
+  {
+    id: 2,
+    title: "Best post in the world",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
+    slug: "post2",
+    created_at: "202212012004",
+    updated_at: "202212012004",
+  },
+  {
+    id: 3,
+    title: "Best post in the world",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
+    slug: "post3",
+    created_at: "202212012004",
+    updated_at: "202212012004",
+  },
+  {
+    id: 4,
+    title: "Best post in the world",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
+    slug: "post4",
+    created_at: "202212012004",
+    updated_at: "202212012004",
+  },
+  {
+    id: 5,
+    title: "Best post in the world",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget rhoncus nisi. Donec lacinia turpis massa, nec suscipit nibh mollis et. Aenean nec sem commodo, dignissim nulla vel, tincidunt tortor. In tempus eget ante at bibendum. Nullam convallis, arcu sed hendrerit viverra, lacus ex lacinia leo, sit amet varius dui ex a metus. Curabitur egestas quam quis nulla commodo ultrices. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus in libero quis eros commodo elementum. Maecenas et leo hendrerit, tempus purus non, vehicula erat.",
+    slug: "post5",
+    created_at: "202212012004",
+    updated_at: "202212012004",
+  },
+]
